b82a6e24d48ce7c5a517e23c6ee1efe0c425ff34
Kevin <stradtkt22@gmail.com>
2018-08-14 22:27:33 -0400

    added login register and fa

--- apps/ccrs/urls.py
+++ apps/ccrs/urls.py
@@ -3,4 +3,9 @@ from . import views
 
 urlpatterns = [
     url(r'^$', views.index),
+    url(r'^login$', views.login),
+    url(r'^register$', views.register),
+    url(r'^logout$', views.logout),
+    url(r'^login-page$', views.login),
+    url(r'^register-page$', views.register_page),
 ]
--- apps/ccrs/views.py
+++ apps/ccrs/views.py
@@ -6,3 +6,47 @@ from django.shortcuts import render, redirect
 # Create your views here.
 def index(request):
     return render(request, 'ccrs/index.html')
+
+def login(request):
+    email = request.POST['email']
+    password = request.POST['password']
+    user = User.objects.filter(email=email)
+    if len(user) > 0:
+        is_pass = bcrypt.checkpw(password.encode(), user[0].password.encode())
+        if is_pass:
+            request.session['id'] = user[0].id
+            messages.success(request, 'Logged In!')
+            return redirect('/')
+        else:
+            messages.error(request, "Incorrect email and/or password")
+            return redirect('/')
+    else:
+        messages.error(request, "User does not exist")
+    return redirect('/')
+
+def register(request):
+    errors = User.objects.validate_user(request.POST)
+    if len(errors):
+        for tag, error in errors.iteritems():
+            messages.error(request, error)
+        return redirect('/')
+    else:
+        first_name = request.POST['first_name']
+        last_name = request.POST['last_name']
+        email = request.POST['email']
+        dob = request.POST['dob']
+        password = request.POST['password']
+        hashed_pw = bcrypt.hashpw(password.encode(), bcrypt.gensalt())
+        User.objects.create(name=name, alias=alias, email=email, dob=dob, password=hashed_pw)
+        messages.success(request, 'User Registered')
+        return redirect('/')
+
+def logout(request):
+    request.session.clear()
+    return redirect('/')
+
+def reqister_page(request):
+    return render(request, 'ccrs/register.html')
+
+def login_page(request):
+    return render(request, 'ccrs/login.html')
